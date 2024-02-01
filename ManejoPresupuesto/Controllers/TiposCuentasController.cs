383cdfdc148bcfb9dc247d557e5bf7adf303ccff
Victor <VictorCarmona9035@hotmail.com>
2024-02-01 21:33:09 +0100

    Comunicandonos con la base de datos - Connection Strings

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,16 +1,29 @@
-﻿using ManejoPresupuesto.Models;
+﻿using Dapper;
+using ManejoPresupuesto.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ManejoPresupuesto.Controllers
 {
     public class TiposCuentasController: Controller
     {
 
+        private readonly string connectionString;
+        public TiposCuentasController(IConfiguration configuration) 
+        {
+            connectionString = configuration.GetConnectionString("RutaServerSQL");
+        }
         public IActionResult Crear()
         {
-            //var modelo = new TipoCuenta() { Nombre = "pedro" };
-            //return View(modelo);
-            return View();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+                    
+            }
+                //var modelo = new TipoCuenta() { Nombre = "pedro" };
+                //return View(modelo);
+                return View();
         }
 
         [HttpPost]
