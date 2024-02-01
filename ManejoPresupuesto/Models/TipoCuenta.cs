84a2e8e09af96f3cc8ffb29b65b187821cdb18fd
victorm9035 <victorcarmona9035@hotmail.com>
2024-02-01 08:03:50 +0100

    Otras Validaciones por Defecto

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -11,5 +11,17 @@ namespace ManejoPresupuesto.Models
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+        /*Prueba de otra validaciones por defecto*/
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage ="El campo debe ser un correo electronico válido")]
+        public string Email { get; set; }
+        [Range(minimum:19, maximum:130, ErrorMessage ="El valor debe estar entre {1} y {2}")]
+        public int Edad { get; set; }
+        [Url(ErrorMessage ="El campo debe ser una URL valida")]
+        public string Url { get; set; }
+        [CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        [Display(Name ="Tarjeta de Credito")]
+        public string TarjetaCredito { get; set; }
     }
 }
