9842f96f3cab415c113aa6440c4d6843430871cf
Victor <VictorCarmona9035@hotmail.com>
2024-02-01 19:24:20 +0100

    Validaciones personalizadas por Atributo

---
+++ ManejoPresupuesto/Validaciones/PrimeraLetraMayusculaAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Validaciones
+{
+    public class PrimeraLetraMayusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            //Validamos si es nulo
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            //Obtenemos el primer caracter
+            string primeraLetra = value.ToString()[0].ToString();
+
+            //Si la primera letra es distinta a su version en mayuscula es que no es mayuscula
+            // por lo tanto
+            if (primeraLetra !=primeraLetra.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayuscula");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
