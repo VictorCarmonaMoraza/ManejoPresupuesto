88fc8067112f8196aa00653ec6f33d9c5347112b
Victor <VictorCarmona9035@hotmail.com>
2024-02-09 23:43:41 +0100

    Formulario de Cuentas

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,46 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    /// <summary>
+    /// Controlador encargado de gestionar las operaciones sobre las cuentas en la aplicación.
+    /// </summary>
+    public class CuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas _repositorioTiposCuentas;
+        private readonly IServicioUsuarios _servicioUsuarios;
+
+        /// <summary>
+        /// Inicializa una nueva instancia del controlador CuentasController.
+        /// </summary>
+        /// <param name="repositorioTiposCuentas">Repositorio para operaciones relacionadas con los tipos de cuentas.</param>
+        /// <param name="servicioUsuarios">Servicio para obtener información y operaciones relacionadas con los usuarios.</param>
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServicioUsuarios servicioUsuarios)
+        {
+            _repositorioTiposCuentas = repositorioTiposCuentas;
+            _servicioUsuarios = servicioUsuarios;
+        }
+
+        /// <summary>
+        /// Muestra la vista para la creación de una nueva cuenta, proporcionando los tipos de cuentas disponibles para el usuario actual.
+        /// </summary>
+        /// <returns>
+        /// Retorna la vista 'Crear' junto con un modelo que incluye los tipos de cuentas disponibles para el usuario actual,
+        /// permitiendo seleccionar uno de ellos al crear la cuenta.
+        /// </returns>
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = _servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await _repositorioTiposCuentas.ListarPorUsuarioId(usuarioId);
+            var modelo = new CuentaCreacionViewModel();
+
+            modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+
+            return View(modelo);
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/Cuenta.cs
@@ -0,0 +1,42 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    /// <summary>
+    /// Representa una cuenta financiera en el sistema.
+    /// </summary>
+    public class Cuenta
+    {
+        /// <summary>
+        /// Identificador único de la cuenta.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombre de la cuenta. Es un campo obligatorio, debe tener una longitud máxima de 50 caracteres,
+        /// y su primera letra debe ser mayúscula.
+        /// </summary>
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        [StringLength(maximumLength: 50, ErrorMessage = "La longitud máxima del nombre es de 50 caracteres")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Identificador del tipo de cuenta asociado a esta cuenta. Este campo se muestra como 'Tipo de Cuenta' en la interfaz de usuario.
+        /// </summary>
+        [Display(Name = "Tipo de Cuenta")]
+        public int TipoCuentaId { get; set; }
+
+        /// <summary>
+        /// Balance actual de la cuenta.
+        /// </summary>
+        public decimal Balance { get; set; }
+
+        /// <summary>
+        /// Descripción opcional de la cuenta.
+        /// </summary>
+        public string Descripcion { get; set; }
+    }
+
+}
